e7d0b51e7b48cd358777e29517329574b5eeead3
MaxAkonde <devdevmaster@gmail.com>
2019-11-06 15:03:43 +0100

    init bootstrap design

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import React, {useEffect, useState} from 'react';
-import Recipe from './app/Recipe';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Recipe from "./app/Recipe";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
 
 const App = () => {
-
   const APP_ID = "f7653df1";
   const APP_KEY = "5d66e07f0f78071472441aa2ab24fe8d";
 
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
   const [query, setQuery] = useState("chicken");
-  
+
   useEffect(() => {
     getRecipes();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
     const data = await response.json();
     setRecipes(data.hits);
-  }
+  };
 
   const updateSearch = e => {
     setSearch(e.target.value);
-  }
+  };
 
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
-    setSearch('');
-  }
+    setSearch("");
+  };
 
-  return(
-    <div className="App">
-      <h1>Hello</h1>
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      {recipes.map(recipe => (
-        <Recipe 
-          key={recipe.recipe.label}
-          title={recipe.recipe.label} 
-          calories={recipe.recipe.calories}
-          image={recipe.recipe.image}
-          ingredients={recipe.recipe.ingredients}
-        />
-      ))}
+  return (
+    <div className="">
+      <div className="container-fluid">
+        <h1>Catalog Of Recipes</h1>
+        <form onSubmit={getSearch}>
+          <div className="row">
+            <div className="form-group col-10">
+              <input
+                className="form-control"
+                type="text"
+                value={search}
+                onChange={updateSearch}
+              />
+            </div>
+            <div className="form-group col">
+              <button className="btn btn-primary" type="submit">
+                Search
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div className="container">
+        {recipes.map(recipe => (
+          <Recipe
+            key={recipe.recipe.label}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/app/Recipe.jsx
+++ src/app/Recipe.jsx
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-const Recipe = ({title, calories, image, ingredients}) => {
-    return(
-        <div>
-            <h1>{title}</h1>
+const Recipe = ({ title, calories, image, ingredients }) => {
+  return (
+    <div className="row">
+      <div className="card" style={{ width: "18rem" }}>
+        <img src={image} class="card-img-top" alt="" />
+        <div className="card-body">
+          <h5 className="card-title">{title}</h5>
+          <p className="card-text">{calories}</p>
+          <p className="card-text">
             <ul>
-                {ingredients.map(ingredient => (
-                    <li>{ingredient.text}</li>
-                ))}
+              {ingredients.map(ingredient => (
+                <li>{ingredient.text}</li>
+              ))}
             </ul>
-            <p>{calories}</p>
-            <img src={image} alt=""/>
+          </p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Recipe;
